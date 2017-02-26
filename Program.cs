7716ca3eaecb464e4155507fa3bd297231e4fc25
Taylor Hutchison <seanthutchison@gmail.com>
2017-02-26 17:16:22 -0600

    Text substitution in layouts and saving file/folder structure with stream writer.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Pitch
     class Program
     {
         static IEnumerable<string> FilePaths;
-        //static Dictionary<string, string> Layouts;
+        static Dictionary<string, string> Layouts = new Dictionary<string,string>();
 
         static IEnumerable<string> DirSearch(string directory, Options options)
         {
@@ -32,20 +32,37 @@ namespace Pitch
 
         static string GetLayoutPath(string filePath, string layoutPath)
         {
-            var count = Regex.Matches(Regex.Escape(layoutPath),  @"../").Count;
+            var count = Regex.Matches(Regex.Escape(layoutPath), @"../").Count;
             var relativeFileName = Path.GetFileName(layoutPath);
             var filePathParts = filePath.Split('/');
             return string.Join("/", filePathParts.Take(filePathParts.Count() - count - 1)) + "/" + relativeFileName;
+        }
+
+        static string GetLayoutContents(string layoutPath)
+        {
+            if (Layouts.Keys.Contains(layoutPath))
+            {
+                return Layouts[layoutPath];
+            }
+            var stream = new FileStream(layoutPath, FileMode.Open, FileAccess.Read);
+            var reader = new StreamReader(stream as FileStream, Encoding.UTF8, true, 512);
+            var contents = reader.ReadToEnd();
+            Layouts.Add(layoutPath, contents);
+            return contents;
         }
 
         static void Main(string[] args)
         {
             const string layoutRegex = ".*layout=\"(.*)\"";
             var options = ParseOptions(args);
+            Console.WriteLine($"Options: searchDir = {options.SearchDir}, pattern = {options.Pattern}, outDir = {options.OutDir}, diagnostics = {options.Diagnostics}");
             //Phase 1: Discover
-            Directory.SetCurrentDirectory("test");
+
+            Directory.SetCurrentDirectory(options.SearchDir);
             var cwd = Directory.GetCurrentDirectory();
-            Console.WriteLine($"CWD: {cwd}");
+
+            Directory.CreateDirectory(options.OutDir);
+
             FilePaths = DirSearch(cwd, options);
 
             //Phase 2: Link
@@ -56,15 +73,22 @@ namespace Pitch
                 var line = reader.ReadLine();
                 if (Regex.IsMatch(line, layoutRegex, RegexOptions.IgnoreCase))
                 {
-                    var match = Regex.Match(line, layoutRegex, RegexOptions.IgnoreCase)?.Groups[1]?.Captures[0]?.Value;
-                    var layoutPath = GetLayoutPath(file, match);
-                    Console.WriteLine(layoutPath);
+                    var fileContents = reader.ReadToEnd();
+                    var match = Regex.Match(line, layoutRegex, RegexOptions.IgnoreCase);
+                    var layout = match?.Groups[1]?.Captures[0]?.Value;
+                    var layoutPath = GetLayoutPath(file, layout);
+                    string layoutContents = GetLayoutContents(layoutPath);
+                    var combinedContents = layoutContents.Replace("@layout-outlet", fileContents);
+                    var outPath = options.OutDir + file.Replace(cwd, "");
+                    Directory.CreateDirectory(outPath.Replace(Path.GetFileName(outPath),""));
+                    var outStream = new FileStream(outPath, FileMode.Create, FileAccess.Write);
+                    var writer = new StreamWriter(outStream as FileStream, Encoding.UTF8, 512);
+                    writer.Write(combinedContents);
+                    writer.Flush();
                 }
             }
 
-            //Phase 3: Output
-
-            //Phase 4: Diagnostics
+            //Phase 3: Diagnostics
         }
     }
 }
