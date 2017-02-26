c40182fa5aba3ad9d0d1b49999af62d1e6eaf08e
Taylor Hutchison <seanthutchison@gmail.com>
2017-02-26 17:14:46 -0600

    Added CLI argument parsing.

--- Options.cs
+++ Options.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 
 namespace Pitch {
@@ -16,15 +17,43 @@ namespace Pitch {
             Verbose
         }
 
+        public string SearchDir {get; private set;}
         public Regex Pattern {get; private set;}
         public string OutDir {get; private set;}
         public DiagnosticsLevel Diagnostics {get; private set;}
 
         public static Options ParseOptions(string[] args){
+            var defaultSearchDir = "./";
+            var defaultPattern = "*";
+            var defaultOutDir = "pitch-output";
+            var defaultDiagnostics = DiagnosticsLevel.Warnings;
+
+            for(int i = 0; i < args.Length; i++){
+                if(args[i].ToLower() == "--searchdir" && i < args.Length - 1){
+                    defaultSearchDir = args[i + 1];
+                    i++;
+                }
+                if(args[i].ToLower() == "--pattern" && i < args.Length - 1){
+                    defaultPattern = args[i + 1];
+                    i++;
+                }
+
+                if(args[i].ToLower() == "--outdir" && i < args.Length - 1){
+                    defaultOutDir = args[i + 1];
+                    i++;
+                }
+
+                if(args[i].ToLower() == "--diagnostics" && i < args.Length - 1){
+                    defaultDiagnostics = (DiagnosticsLevel)Int32.Parse(args[i + 1]);
+                    i++;
+                }
+            }
+
             return new Options {
-                Pattern = WildcardToRegex("*.html"),
-                OutDir = "",
-                Diagnostics = DiagnosticsLevel.Warnings
+                SearchDir = defaultSearchDir,
+                Pattern = WildcardToRegex(defaultPattern),
+                OutDir = defaultOutDir,
+                Diagnostics = defaultDiagnostics
             };
         }
 
