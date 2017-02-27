e8f6ce74d09b129e57f6118d1b3c5b4dfee0b447
Taylor Hutchison <seanthutchison@gmail.com>
2017-02-26 18:54:36 -0600

    Added a --help CLI option.

--- Options.cs
+++ Options.cs
@@ -20,6 +20,7 @@ namespace Pitch {
         public string SearchDir {get; private set;}
         public Regex Pattern {get; private set;}
         public string OutDir {get; private set;}
+        public bool Help {get; private set;}
         public DiagnosticsLevel Diagnostics {get; private set;}
 
         public static Options ParseOptions(string[] args){
@@ -27,6 +28,7 @@ namespace Pitch {
             var defaultPattern = "*";
             var defaultOutDir = "pitch-output";
             var defaultDiagnostics = DiagnosticsLevel.Warnings;
+            var defaultHelp = false;
 
             for(int i = 0; i < args.Length; i++){
                 if(args[i].ToLower() == "--searchdir" && i < args.Length - 1){
@@ -47,13 +49,18 @@ namespace Pitch {
                     defaultDiagnostics = (DiagnosticsLevel)Int32.Parse(args[i + 1]);
                     i++;
                 }
+
+                if(args[i].ToLower() == "--help"){
+                    defaultHelp = true;
+                }
             }
 
             return new Options {
                 SearchDir = defaultSearchDir,
                 Pattern = WildcardToRegex(defaultPattern),
                 OutDir = defaultOutDir,
-                Diagnostics = defaultDiagnostics
+                Diagnostics = defaultDiagnostics,
+                Help = defaultHelp
             };
         }
 
